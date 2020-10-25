573b4c6ce6129749470f3dea9fc0faab11c4330d
Alessandro Dolci <alessandro.rn@gmail.com>
2020-10-25 17:27:11 +0100

    chore: modified Webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,32 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
+const baseConfiguration = {
     entry: "./src/index.js",
     output: {
         filename: "main.js"
-    },
-    devServer: {
-        port: 3000
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./index.html"
         })
     ]
+};
+
+const devConfiguration = {
+    ...baseConfiguration,
+    mode: "development",
+    devServer: {
+        port: 3000
+    },
+    devtool: "source-map"
+};
+
+const prodConfiguration = {
+    ...baseConfiguration,
+    mode: "production",
+    devtool: "inline-source-map"
+};
+
+module.exports = env => {
+    return env === "production" ? prodConfiguration : devConfiguration;
 };
